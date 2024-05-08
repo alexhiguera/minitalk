8c3d873917a4a3c51390e99479eb3c14fbc11818
Columbux <ahiguera@student.42urduliz.com>
2024-05-08 21:45:00 +0200

    update: finishing server

--- src/server.c
+++ src/server.c
@@ -6,15 +6,29 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 15:30:28 by alex              #+#    #+#             */
-/*   Updated: 2024/05/08 16:18:57 by alex             ###   ########.fr       */
+/*   Updated: 2024/05/08 16:45:58 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include "libft_2.0/src/libft.h"
 
 void	mt_btoi(int signal)
 {
-	//
+	int	bit;
+	int	i;
+
+	i = 0;
+	bit = 0;
+	if (signal == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		i = 0;
+		bit = 0;
+	}
 }
 
 int	main(int argc, char **argv)
@@ -28,5 +42,7 @@ int	main(int argc, char **argv)
 		signal(SIGUSR2, mt_btoi);
 		pause();
 	}
+	else
+		ft_error("don't add arguments please");
 	return (0);
 }
