d2b85686c7c5940d32fe4d877e61e022aff2b6d4
Columbux <ahiguera@student.42urduliz.com>
2024-05-08 21:45:12 +0200

    update: main function

--- src/client.c
+++ src/client.c
@@ -6,8 +6,18 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 15:29:07 by alex              #+#    #+#             */
-/*   Updated: 2024/05/08 15:30:15 by alex             ###   ########.fr       */
+/*   Updated: 2024/05/08 18:55:23 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include "libft_2.0/src/libft.h"
+
+int	main(int argc, char **argv)
+{
+	pid_t	pid;
+
+	pid = getpid();
+	ft_printf("En proceso");
+	return (0);
+}
