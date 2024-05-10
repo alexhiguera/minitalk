1f8831a53d45cfc6e31b1415c6331edf5bf5a1a7
alexhiguera <alexhigueramartinez@gmail.com>
2024-05-11 00:19:50 +0200

    build: .h created

---
+++ src/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: columbux <columbux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 16:42:02 by ahiguera          #+#    #+#             */
+/*   Updated: 2024/05/09 17:38:01 by columbux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include "../libft_2.0/src/libft.h"
+
+void	mt_btoi(int signal);
+
+#endif
